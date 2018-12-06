3e47dfcf103d23e0baebfe3dc2aa3b4d7203f7f0
whdghks8021 <whdghks8021@naver.com>
2018-12-06 15:29:16 +0900

    user생성/권한부여/Table CRUD/2018.12.06

--- 관리자2018-12-06.sql
+++ 관리자2018-12-06.sql
@@ -15,3 +15,16 @@ CREATE USER user1 IDENTIFIED BY 1234;
 -- GRANT, REVOKE
 GRANT CONNECT TO user1; -- user1에게 로그인 권한만 부여
 REVOKE CONNECT FROM user1; -- user1에게 로그인 권한 제거
+
+CREATE USER user2 IDENTIFIED BY 1234;
+GRANT CONNECT TO user2;
+
+-- CREATE 명령으로 생성된 객체는 삭제할때 DROP 키워드로 삭제한다.
+DROP USER user2;
+-- 한번 삭제된 사용자는 그 내용을 복구 할 수 없다.
+
+-- 오라클 학습 환경에서 다양한 명령들을 수행하고 테스트 해 보기 위해서
+-- 우리는 새로운 사용자에게 SYSDBA보다 한단계 낮은 DBA권한을 부여해서
+-- 학습을 진행한다.
+GRANT DBA TO user1;
+--- DBA : DDL DML, DCL 명령들을 수행할 수 있다.
