89bca4b000b47809239fbe73cb39e7841076a9cb
whdghks8021 <whdghks8021@naver.com>
2019-01-14 10:40:06 +0900

    UNIQUE 제약조건 / FOREIGN KEY / 참조무결성

---
+++ 퀴즈메이커(2019.01.10).sql
@@ -0,0 +1,12 @@
+-- 여기는 myQuiz 입니다.
+
+CREATE TABLE tbl_cbt (
+    cb_num NUMBER PRIMARY KEY,
+    cb_problem nVARCHAR2(255) NOT NULL,
+    cb_answer nVARCHAR2(255) NOT NULL,
+    cb_nanswer1 nVARCHAR2(255) NOT NULL,
+    cb_nanswer2 nVARCHAR2(255) NOT NULL,
+    cb_nanswer3 nVARCHAR2(255) NOT NULL
+);
+
+DROP TABLE tbl_quiz ;
